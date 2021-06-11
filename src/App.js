552f254b2ac35e6b94576daee0965fc4e3a167fe
Sreenikhil <kollu1996@gmail.com>
2021-06-11 15:51:08 -0400

    Initial React Code

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import '../src/css/Skills.css';
+
+function Skills() {
+  return (
+      <div>
+      <div className="ui cards">
+          <div className="card" id= "cardbox">
+              <div className="content" id="content">
+                  <div className="skillName">React</div>
+              </div>
+          </div>
+      </div>
+      </div>
+  );
+}
+
+export default Skills;
